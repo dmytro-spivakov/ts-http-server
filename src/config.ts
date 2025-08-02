b44086070133f4f2d823a1eaa81b5f5b5bbed767
Dmytro Spivakov <dmytro_spivakov@proton.me>
2025-08-02 23:20:12 +0300

    add drizzle orm and pg config, commit db schema

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,22 @@
+process.loadEnvFile();
+
 type APIConfig = {
+	dbURL: string;
 	fileserverHits: number;
 };
 
 const config: APIConfig = {
+	dbURL: envOrThrow("DB_URL"),
 	fileserverHits: 0,
+}
+
+function envOrThrow(key: string): string {
+	const envVal = process.env[key];
+	if (typeof envVal === "string") {
+		return envVal;
+	}
+
+	throw new Error(`ENV VARIABLE MISSING - ${key}`);
 }
 
 export { config };
